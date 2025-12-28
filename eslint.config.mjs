3ab9a73c14eb65273748317c23322db20c840c23
KamillaKhabirakhmanova <venessiel@gmail.com>
2025-12-27 22:33:43 -0500

    Fix ESLint config - update flat config format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,11 @@
 import studio from '@sanity/eslint-config-studio'
 
-export default [...studio]
+export default [
+  ...studio,
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+]
